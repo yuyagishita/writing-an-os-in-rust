6bfdd4695f9c592c5773ac6815668aff49480b7e
yuyagishita <yu.yagishita1995@gmail.com>
2022-02-13 01:15:01 +0900

    feat: rustでつくる最小のカーネル

--- blog_os/src/main.rs
+++ blog_os/src/main.rs
@@ -3,15 +3,25 @@
 
 use core::panic::PanicInfo;
 
-#[no_mangle] // この関数の名前修飾をしない
-pub extern "C" fn _start() -> ! {
-    // リンカはデフォルトで `_start` という名前の関数を探すので、
-    // この関数がエントリポイントとなる
-    loop {}
-}
-
 /// この関数はパニック時に呼ばれる
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+
+static HELLO: &[u8] = b"Hello World!";
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
