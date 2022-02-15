ec52edba93121a04ca4ce74dc7310b8435e61b90
yuyagishita <yu.yagishita1995@gmail.com>
2022-02-15 22:10:59 +0900

    feat: テストの途中

--- blog_os/src/main.rs
+++ blog_os/src/main.rs
@@ -1,8 +1,19 @@
 #![no_std] // Rust の標準ライブラリにリンクしない
 #![no_main] // 全ての Rust レベルのエントリポイントを無効にする
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 mod vga_buffer;
 use core::panic::PanicInfo;
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
 
 /// この関数はパニック時に呼ばれる
 #[panic_handler]
@@ -16,7 +27,15 @@ static HELLO: &[u8] = b"Hello World!";
 #[no_mangle]
 pub extern "C" fn _start() {
     println!("Hello World{}", "!");
-    panic!("Some panic message");
+    #[cfg(test)]
+    test_main();
 
     loop {}
+}
+
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion... "); // "些末なアサーション……"
+    assert_eq!(1, 1);
+    println!("[ok]");
 }
