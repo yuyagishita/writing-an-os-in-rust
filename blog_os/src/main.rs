8ecb8a3824f82a949591fcbd7f2b12466abc3743
yuyagishita <yu.yagishita1995@gmail.com>
2022-03-03 23:04:34 +0900

    feat: 作ってるosからホストのコンソールに表示をしようとしている。

--- blog_os/src/main.rs
+++ blog_os/src/main.rs
@@ -4,6 +4,7 @@
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
+mod serial;
 mod vga_buffer;
 use core::panic::PanicInfo;
 
@@ -13,6 +14,7 @@ fn test_runner(tests: &[&dyn Fn()]) {
     for test in tests {
         test();
     }
+    exit_qemu(QemuExitCode::Success);
 }
 
 /// この関数はパニック時に呼ばれる
@@ -38,4 +40,21 @@ fn trivial_assertion() {
     print!("trivial assertion... "); // "些末なアサーション……"
     assert_eq!(1, 1);
     println!("[ok]");
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+    mod serial;
 }
